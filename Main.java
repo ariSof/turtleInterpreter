532f2a9ca350f2934f19de9c5cdfdb118d38ce5e
prieta <sofiaboubion@gmail.com>
2025-12-06 01:01:29 -0800

    fixed issue with visitor

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         FileReader reader = new FileReader();
         reader.setFileName("turtleProgram.txt");
 
-        //Visitor visitor = new ConcreteTraversalVisitor();
+        Visitor visitor = new ConcreteTraversalVisitor();
         ConcreteTraversalVisitor traversalVisitor = new ConcreteTraversalVisitor();
         Turtle myTurtle = new Turtle();
         ArrayList<String> commands = reader.readFile();
@@ -45,11 +45,11 @@ public class Main {
                 System.out.println("Error reading your file.");
             }
 
-           // theCommand.accept(visitor);
+            theCommand.accept(traversalVisitor);
             theCommand.interpret(distance);
         }
         System.out.println(myTurtle.toString());
-        System.out.println(traversalVisitor.toString());
+        System.out.println("Traversal="+traversalVisitor.toString());
 
         TotalDistanceVisitor distanceVisitor = new TotalDistanceVisitor();
         for (AbstractCommand cmd: traversalVisitor.getMementos()){
@@ -59,8 +59,8 @@ public class Main {
 
 
         //Turtle check after:
-       // System.out.println(myTurtle.toString());
-       // System.out.println(visitor.toString());
+        System.out.println(myTurtle.toString());
+        System.out.println(visitor.toString());
        System.out.println(distanceVisitor.toString());
     }
 }
